a2aad7d20a1a2e1285a7876d14c0b430468c1540
Emil <emilrakh@gmail.com>
2020-12-17 19:39:55 +0500

    also smth new

---
+++ src/main/java/prototype/RegistrarionHospitalSystem/RegistrationHospitalSystemApplication.java
@@ -0,0 +1,12 @@
+package prototype.RegistrarionHospitalSystem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationHospitalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationHospitalSystemApplication.class, args);
+    }
+
+}
